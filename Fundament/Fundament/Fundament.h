a83901f02d9438e6499cb91f891ecc97349d8466
Jon Packer <contact@jonpacker.com>
2011-12-01 00:15:11 +1000

    Added timers and listeners

--- Fundament/Fundament/Fundament.h
+++ Fundament/Fundament/Fundament.h
@@ -8,6 +8,13 @@
 #import <Foundation/Foundation.h>
 
 /**
+ * The default update duration (in seconds). If you don't specify an update duration, this fallback will be used. Note
+ * that this is the fallback for the instance property defaultUpdateDuration. If you don't set that, that is when this 
+ * value will be used. If you set it, it will override this.
+ */
+#define FundamentDefaultUpdateDuration 60
+
+/**
  * The default config will contain a list of data sources to load by default on the shared fundament instance. They
  * will be loaded immediately as Fundament is (when +initialize is called). Format (obviously this can only be URL
  * data sources):
@@ -26,41 +33,72 @@
 
 // Everything traces back to FundamentDataSourceBlock, pretty much. So it's pretty important. But it's nothing too 
 // special. It just takes a callback argument (can you tell I use node much?) as a FundamentSuccessBlock - which just 
-// takes an (id) as your data to store. Got an error? No problem! Just ignore the callback.
+// takes an (id) as your data to store. 
 typedef void (^FundamentSuccessBlock)(id data);
 typedef void (^FundamentDataSourceBlock)(FundamentSuccessBlock success);
+
+// Block type that is used as a callback when data is updated.
+typedef void (^FundamentCallback)(id data);
 
 // These are the types of responses that we can receive. Of course, you can always plug in your own. No problem. This is
 // just to make your life easier so you don't have to write the 1 line to parse some JSON!
 typedef enum {
-  FundamentResponseTypeJSON // corresponding string = "json"
+  FundamentResponseTypeJSON,    // corresponding name = "json". Uses JSONKit to convert JSON to an NSDictionary/NSArray
+  FundamentResponseTypeString,  // corresponding name = "string". Returns the raw output string.
+  FundamentResponseTypeData,    // corresponding name = "data". Returns the raw NSData.
+  FundamentResponseTypePlist,   // corresponding name = "plist". Uses built-in methods to read data as a plist.
+  FundamentResponseTypeImage    // corresponding name = "image. Reads data as an image. 
 } FundamentResponseType;
+
+// These store the status of a timer. A timer can be busy, or idle. The timer is busy when it is making its call, and 
+// becomes idle straight after until the next call.
+typedef enum {
+  FundamentTimerStatusIdle = 0,
+  FundamentTimerStatusBusy = 1
+} FundamentTimerStatus;
 
 @interface Fundament : NSObject {
  @protected
-  // Blocks are stored in here
-  NSMutableDictionary* _dataSources;
+  // Timers are stored in here. Blocks, listeners and statuses are stored in their userInfo.
+  NSMutableDictionary* _timers;
   
   // Result of data source calls are stored in here. Since it's an NSCache the existence of a value isn't guaranteed,
   // which results in the need for all our data request calls to be async.
   NSCache* _dataCache;
+  
+ @private
+  // Default time interval. If set to 0, #FundamentDefaultUpdateDuration will be used.
+  NSTimeInterval _defaultUpdateInterval;
 }
+
+#pragma mark - Class Methods
  
 // Access the shared instance of Fundament. 
 + (Fundament *) sharedFundament;
 
-// Add the given data-source using a block that returns some data.
+#pragma mark - Properties
+
+// This value will be used when no value is specified.
+@property (nonatomic, assign) NSTimeInterval defaultUpdateInterval;
+
+#pragma mark - Instance Methods
+
+// Add the given data-source using a block that returns some data. Will be fired according to updateInterval.
+- (void) addDataSource:(FundamentDataSourceBlock)dataSource withUpdateInterval:(NSTimeInterval)updateInterval 
+                forKey:(NSString *)key;
+
+// Add the given data-source using a block that returns some data. Update interval will be set according to the default.
 - (void) addDataSource:(FundamentDataSourceBlock)dataSource forKey:(NSString *)key;
 
 // Add the given data-source, but generate a unique key for it, and return that. Each method will have one of these
-// variants from now on, but no need for them to be documented as well.
+// variants from now on, but no need for them to be documented as well. Update interval will be set to the default.
 - (NSString *) addDataSource:(FundamentDataSourceBlock)dataSource;
 
 // Add the URL as a data-source, expecting the given response type.
 - (void) addURLDataSource:(NSURL *)dataSource 
          withResponseType:(FundamentResponseType)responseType 
+           updateInterval:(NSTimeInterval)updateInterval
                    forKey:(NSString *)key;
-- (NSString *) addURLDataSource:(NSURL *)dataSource withResponseType:(FundamentResponseType)responseType;
 
 // Add URL data source from an NSDictionary. Should contain two keys, "format", and "url". "format" being one of the
 // formats that translate to FundamentResponseType. See its documentation for corresponding strings.
@@ -70,5 +108,11 @@ typedef enum {
 // Add a bunch of URL data sources from an NSDictionary. Can contain any number of keys you want, the keys being the 
 // identifiers that you will retrieve and observe the data with.
 - (void) addURLDataSourcesWithDictionary:(NSDictionary *)dictionary;
+
+// Add a listener for the given key using a block
+- (void) addListenerWithBlock:(FundamentCallback)listener forKey:(NSString *)key;
+
+// Add a listener for the given key using a target & selector
+- (void) addListenerWithTarget:(id)target selector:(SEL)selector forKey:(NSString *)key;
 
 @end
--- Fundament/Fundament/Fundament.m
+++ Fundament/Fundament/Fundament.m
@@ -7,6 +7,7 @@
 
 #import "Fundament.h"
 #import "JSONKit.h"
+#import "ASIHTTPRequest.h"
 
 #pragma mark - Logging
 
@@ -29,6 +30,13 @@ static inline void FundamentLog(UInt8 level, NSString* format, ...) {
 #endif 
 }
 
+#pragma mark - Info Keys
+
+#define FundamentDataSourceKey @"dataSource"
+#define FundamentListenersKey @"listeners"
+#define FundamentStatusKey @"status"
+#define FundamentKeyKey @"key"
+
 #pragma mark - UUID Generation
 
 static NSString* FundamentCreateUUID() {
@@ -38,6 +46,37 @@ static NSString* FundamentCreateUUID() {
   
   return uuidStr;
 }
+
+#pragma mark - Internal Listener Class Base
+
+// This is the base listener class that allows us the store listeners in an abstract way inside a dictionary quite
+// easily. 
+@interface FundamentListener : NSObject 
+
+- (void) callWithData:(id)data; // Call this listener.
+
+@end
+
+#pragma mark Internal Listener - Block
+
+@interface FundamentBlockListener : FundamentListener  
+
+@property (nonatomic, copy) FundamentCallback block;
+
+- (id) initWithBlock:(FundamentCallback)block;
+
+@end
+
+#pragma mark Internal Listener - Target & Sel
+
+@interface FundamentTargetSelListener : FundamentListener 
+
+@property (nonatomic, assign) id target;
+@property (nonatomic, assign) SEL selector;
+
+- (id) initWithTarget:(id)target selector:(SEL)sel;
+
+@end
 
 #pragma mark - Private Methods
 
@@ -53,8 +92,28 @@ static NSString* FundamentCreateUUID() {
 // Will load data sources from the default config file.
 - (id) initAsShared;
 
+// Adds a timer to the collection, and optionally executes it immediately.
+- (void) addTimer:(NSTimer *)timer forKey:(NSString *)key andExecuteImmediately:(BOOL)executeImmediately;
+
+// Creates a timer
+- (NSTimer *) timerForDataSource:(FundamentDataSourceBlock)dataSource forKey:(NSString *)key 
+              withUpdateInterval:(NSTimeInterval)updateInterval;
+
 // Data source creator for a JSON data source
-- (void) addJSONDataSource:(NSURL *)dataSource withResponseType:(FundamentResponseType)responseType;
+- (void) addJSONDataSource:(NSURL *)dataSource withUpdateInterval:(NSTimeInterval)updateInterval 
+                    forKey:(NSString *)key;
+
+// Called when a timer fires, delegates the call off to where it should go.
+- (void) timerFired:(NSTimer *)timer;
+
+// Refreshes the data source contained in the given info.
+- (void) refreshDataSourceWithInfo:(NSMutableDictionary *)info;
+
+// Return the info for a certain data source's key
+- (NSMutableDictionary *) infoForDataSourceWithKey:(NSString *)key;
+
+// Return that status for a certain data source
+- (FundamentTimerStatus) statusForDataSourceWithKey:(NSString *)key;
 
 @end
 
@@ -72,6 +131,8 @@ static Fundament* sharedFundament = nil;
   return sharedFundament ? sharedFundament : (sharedFundament = [[Fundament alloc] initAsShared]);
 }
 
+#pragma mark - Property Synthesis
+
 #pragma mark - Object Lifecycle
 
 - (id) init {
@@ -81,15 +142,15 @@ static Fundament* sharedFundament = nil;
   
   [self createNotificationObservers];
   
-  _dataSources = [[NSMutableDictionary alloc] init];
-  _dataCache = [[NSCache alloc] init];
+  _timers         = [[NSMutableDictionary alloc] init],
+  _dataCache      = [[NSCache alloc] init];
   
   return self;
 }
 
 - (void) dealloc {
-  [_dataSources release], _dataSources = nil;
-  [_dataCache release], _dataCache = nil;
+  [_timers release],        _timers = nil;
+  [_dataCache release],     _dataCache = nil;
   
   [super dealloc];
 }
@@ -115,11 +176,91 @@ static Fundament* sharedFundament = nil;
   FundamentLog(2, @"Application Did Enter Background");
 }
 
+#pragma mark - Timers
+
+- (NSTimeInterval) defaultUpdateInterval {
+  return _defaultUpdateInterval > 0 ? _defaultUpdateInterval : FundamentDefaultUpdateDuration;
+}
+
+- (void) setDefaultUpdateInterval:(NSTimeInterval)defaultUpdateInterval {
+  _defaultUpdateInterval = defaultUpdateInterval;
+}
+
+- (void) addTimer:(NSTimer *)timer forKey:(NSString *)key andExecuteImmediately:(BOOL)executeImmediately {
+  [_timers setObject:timer forKey:key];
+  if (executeImmediately) {
+    [timer fire];
+  }
+}
+
+- (NSTimer *) timerForDataSource:(FundamentDataSourceBlock)dataSource forKey:(NSString *)key 
+              withUpdateInterval:(NSTimeInterval)updateInterval {
+  NSMutableDictionary* userInfo = [NSMutableDictionary dictionary];
+  
+  [userInfo setObject:[[dataSource copy] autorelease] forKey:FundamentDataSourceKey];
+  [userInfo setObject:[NSMutableArray array] forKey:FundamentListenersKey];
+  [userInfo setObject:[NSNumber numberWithInt:FundamentTimerStatusIdle] forKey:FundamentStatusKey];
+  [userInfo setObject:[key copy] forKey:FundamentKeyKey];
+  
+  return [NSTimer scheduledTimerWithTimeInterval:updateInterval target:self selector:@selector(timerFired:) 
+                                        userInfo:userInfo repeats:YES];
+}
+
+- (void) timerFired:(NSTimer *)timer {
+  [self refreshDataSourceWithInfo:(NSMutableDictionary *)timer.userInfo];
+}
+
+- (void) refreshDataSourceWithInfo:(NSMutableDictionary *)info {
+  [info setObject:[NSNumber numberWithInt:FundamentTimerStatusBusy] forKey:FundamentStatusKey];
+  
+  FundamentDataSourceBlock dataSource = [info objectForKey:FundamentDataSourceKey];
+  FundamentSuccessBlock success = ^(id data) {
+    NSArray* listeners = [info objectForKey:FundamentListenersKey];
+    for (FundamentListener* listener in listeners) {
+      [listener callWithData:data];
+    }
+    
+    [info setObject:[NSNumber numberWithInt:FundamentTimerStatusIdle] forKey:FundamentStatusKey];
+  };
+  
+  dataSource(success);
+}
+
+#pragma mark - Listeners
+
+//TODO - these will have to return some kind of opaque object. no other way for the user to remove them
+- (void) addListenerWithBlock:(FundamentCallback)listener forKey:(NSString *)key {
+  NSMutableArray* listeners = [[self infoForDataSourceWithKey:key] objectForKey:FundamentListenersKey];
+  [listeners addObject:[[[FundamentBlockListener alloc] initWithBlock:listener] autorelease]];
+}
+
+- (void) addListenerWithTarget:(id)target selector:(SEL)selector forKey:(NSString *)key {
+  NSMutableArray* listeners = [[self infoForDataSourceWithKey:key] objectForKey:FundamentListenersKey];
+  [listeners addObject:[[[FundamentTargetSelListener alloc] initWithTarget:target selector:selector] autorelease]];
+}
+
 #pragma mark - Data Sources
+#pragma mark Existing Info
+
+- (NSMutableDictionary *) infoForDataSourceWithKey:(NSString *)key {
+  NSTimer* timer = [_timers objectForKey:@"key"];
+  return timer ? (NSMutableDictionary *) timer.userInfo : nil;
+}
+
+- (FundamentTimerStatus) statusForDataSourceWithKey:(NSString *)key {
+  return [[[self infoForDataSourceWithKey:key] objectForKey:FundamentStatusKey] intValue];
+}
+
 #pragma mark Generic
 
+- (void) addDataSource:(FundamentDataSourceBlock)dataSource withUpdateInterval:(NSTimeInterval)updateInterval 
+                forKey:(NSString *)key {
+  NSTimer* timer = [self timerForDataSource:dataSource forKey:key withUpdateInterval:updateInterval];
+  [self addTimer:timer forKey:key andExecuteImmediately:YES];
+}
+
 - (void) addDataSource:(FundamentDataSourceBlock)dataSource forKey:(NSString *)key {
-  [_dataSources setObject:[[dataSource copy] autorelease] forKey:key];
+  [self addDataSource:dataSource withUpdateInterval:self.defaultUpdateInterval forKey:key];
 }
 
 - (NSString *) addDataSource:(FundamentDataSourceBlock)dataSource {
@@ -132,14 +273,88 @@ static Fundament* sharedFundament = nil;
 
 - (void) addURLDataSource:(NSURL *)dataSource 
          withResponseType:(FundamentResponseType)responseType 
+           updateInterval:(NSTimeInterval)updateInterval
                    forKey:(NSString *)key {
-  
+  switch (responseType) {
+    case FundamentResponseTypeJSON: {
+      [self addJSONDataSource:dataSource withUpdateInterval:updateInterval forKey:key];
+    } break;
+    default: NSAssert(NO, @"Tried to add an unsupported responseType - %@", responseType);
+  }
 }
 
 # pragma mark JSON
 
-- (void) addJSONDataSource:(NSURL *)dataSource withResponseType:(FundamentResponseType)responseType {
-  
+- (void) addJSONDataSource:(NSURL *)dataSource withUpdateInterval:(NSTimeInterval)updateInterval 
+                    forKey:(NSString *)key {
+  FundamentDataSourceBlock dataSourceBlock = ^(FundamentSuccessBlock success) {
+    __block ASIHTTPRequest* request = [[ASIHTTPRequest alloc] initWithURL:dataSource];
+    [request setCompletionBlock:^{
+      success([request.responseData objectFromJSONData]);
+    }];
+    [request setFailedBlock:^{
+      success(nil);
+    }];
+    [[request autorelease] startAsynchronous];
+  };
+  [self addDataSource:dataSourceBlock withUpdateInterval:updateInterval forKey:key];
 }
 
 @end
+
+#pragma mark - Internal Listener Implementatins
+#pragma mark Base
+
+@implementation FundamentListener
+
+- (void) callWithData:(id)data {
+  NSAssert(NO, @"Erroneous use of FundamentListener base class");
+}
+
+@end
+
+#pragma mark Block
+
+@implementation FundamentBlockListener 
+
+@synthesize block = _block;
+
+- (id) initWithBlock:(FundamentCallback)block {
+  if ( !(self = [super init]) ) return nil;
+  self.block = block;
+  return self;
+}
+
+- (void) callWithData:(id)data {
+  if (!!self.block) self.block(data);
+}
+
+- (void) dealloc {
+  [_block release], _block = nil;
+  [super dealloc];
+}
+
+@end
+
+#pragma mark Target&Sel
+
+@implementation FundamentTargetSelListener
+
+@synthesize target = _target;
+@synthesize selector = _selector;
+
+- (id) initWithTarget:(id)target selector:(SEL)sel {
+  if ( !(self = [super init]) ) return nil;
+  self.target = target, self.selector = sel;
+  return self;
+}
+
+- (void) callWithData:(id)data {
+  if (self.target && [self.target respondsToSelector:self.selector]) {
+    [self.target performSelector:self.selector withObject:data];
+  }
+}
+
+@end
+
+
